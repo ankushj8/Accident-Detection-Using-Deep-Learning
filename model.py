a343996dcb45ac878db8d3c1838068f5b80ffc14
ritwik-singh <31265500+ritwik-singh@users.noreply.github.com>
2019-05-07 16:12:59 +0530

    added labels function

--- model.py
+++ model.py
@@ -3,6 +3,42 @@ from keras.models import Model
 from keras.layers import Input,Dense,TimeDistributed
 from keras.layers import LSTM
 
+
+
+
+
+import glob
+import numpy as np
+
+
+
+
+img_filepath = "/home/ritwik/Desktop/MINI PROJECT/"
+pos = glob.glob(img_filepath + '99frames/*.mp4')
+neg = glob.glob(img_filepath + 'negative/*.mp4')
+all_files =  np.concatenate((pos, neg))
+print(len(neg),len(pos))
+print(all_files)       
+
+
+def label_matrix(values):
+    
+    n_values = np.max(values) + 1    
+    return np.eye(n_values)[values] 
+
+labels = np.concatenate(([1]*len(pos), [0]*len(neg[0:len(pos)])))  
+labels = label_matrix(labels)    
+print(len(labels))      
+
+
+
+
+
+
+
+
+
+
 batch_size = 15
 num_classes = 2
 epochs = 30
